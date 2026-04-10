a056e8f279c6faab53234df8deb121ac0572ff03
Thanqnv <161539907+Thanqnv@users.noreply.github.com>
2026-04-10 15:38:15 +0700

    feature/appbar-navigation

--- app/src/main/java/android/app/producthunt/ui/navigation/Route.kt
+++ app/src/main/java/android/app/producthunt/ui/navigation/Route.kt
@@ -1,9 +1,15 @@
 package android.app.producthunt.ui.navigation
 
 object Route {
-    const val LOGIN: String = "login"
-    const val SIGNUP: String = "signup"
-    const val FORGOT_PASSWORD: String = "forgot_password"
-    const val VERIFY_OTP: String = "verify_otp"
-    const val RESET_PASSWORD: String = "reset_password"
+    const val LOGIN = "login"
+    const val SIGNUP = "signup"
+    const val FORGOT_PASSWORD = "forgot_password"
+    const val VERIFY_OTP = "verify_otp"
+    const val RESET_PASSWORD = "reset_password"
+    
+    const val HOME = "home"
+    const val TRENDING = "trending"
+    const val WISHLIST = "wishlist"
+    const val ALERTS = "alerts"
+    const val PROFILE = "profile"
 }
