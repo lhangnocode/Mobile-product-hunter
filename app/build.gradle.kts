46ed93044a29abcecafe8b660c926219cb7da67b
Thanqnv <161539907+Thanqnv@users.noreply.github.com>
2026-05-31 11:38:15 +0700

    feat: clean up project by removing unused components and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,10 +57,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.ui)
     implementation(libs.androidx.compose.foundation.layout)
-    implementation(libs.androidx.compose.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -87,11 +84,9 @@ dependencies {
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
     implementation(libs.androidx.datastore.preferences)
-    implementation(libs.androidx.datastore.preferences.rxjava3)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.coil.compose)
-    implementation(kotlin("reflect"))
 
     implementation("com.google.ai.edge.litertlm:litertlm-android:latest.release")
 }
